3830fd7f7d252573956237b08272dd74c88040bb
18895007 <mafedyakov@sberbank.ru>
2021-09-03 15:03:04 +0300

    fixed README.md; added benchmarks

--- README.md
+++ README.md
@@ -1,14 +1,20 @@
-Собрать уровень стакан можно при помощи:
+Можно запихнуть в CLion и он подхватит и собрать то что нужно.
+В предыдущей версии я вам отправил версию с гитом, тут её не будет, т.к. там есть лишние вещи.
+Напирмер, там я ещё попробовал abseil либу юзать (гугловый аналог stl).
+Решил его не прикладывать, в общем, если надо - пишите - отдам)
+
+
+Собрать стакан можно при помощи:
 
 cmake . -B cmake-build-debug
 
 cmake --build cmake-build-debug -- -j16
 
-launch specific testcase
+Запустить специфичный тесткейс:
 ./stock_tests --gtest_filter=<testcase_name>
 
-get all test cases
+Список тестов:
 ./stock_tests --gtest_list_tests
 
-to see the commands list that will be executed
+Сгенерить список команд:
 cmake . -B cmake-build-debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON 
--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -3,6 +3,8 @@
 
 #include "benchmark/benchmark.h"
 
+using namespace stl;
+
 void add_asks_stl(benchmark::State& state)
 {
     OrderBook l2;
@@ -30,6 +32,63 @@ void add_mixed_stl(benchmark::State& state)
         }
     }
 }
+void create_then_close_orders_stl(benchmark::State& state)
+{
+    OrderBook l2;
+    std::vector<OfferId> ids;
+    std::vector<Qty> qtys;
+
+    while (state.KeepRunning()) {
+        for (int i = 0; i < 2000000; i++) {
+            Price price = rand() % 1 + 50;
+            Price quantity = rand() % 1 + 40;
+            ids.push_back(i);
+            qtys.push_back(quantity);
+            l2.add_order(OFFER::ASK, price, quantity);
+        }
+
+        for (int i = 0; i < ids.size(); i++) {
+            l2.close_order(ids[i], qtys[i]);
+        }
+    }
+}
+
+void store_stl(benchmark::State& state)
+{
+    OrderBook l2;
+    std::vector<OfferId> ids;
+    std::vector<Qty> qtys;
+
+    while (state.KeepRunning()) {
+        for (int i = 0; i < 2000000; i++) {
+            Price price = rand() % 1 + 50;
+            Price quantity = rand() % 1 + 40;
+            ids.push_back(i);
+            qtys.push_back(quantity);
+            l2.add_order(OFFER::ASK, price, quantity);
+        }
+        l2.store();
+    }
+}
+
+void store_then_load_stl(benchmark::State& state)
+{
+    OrderBook l2;
+    std::vector<OfferId> ids;
+    std::vector<Qty> qtys;
+
+    while (state.KeepRunning()) {
+        for (int i = 0; i < 2000000; i++) {
+            Price price = rand() % 1 + 50;
+            Price quantity = rand() % 1 + 40;
+            ids.push_back(i);
+            qtys.push_back(quantity);
+            l2.add_order(OFFER::ASK, price, quantity);
+        }
+        l2.store();
+        l2.load();
+    }
+}
 
 void add_asks_abseil(benchmark::State& state)
 {
@@ -46,7 +105,13 @@ void add_asks_abseil(benchmark::State& state)
 // faced an issue with big number of iterations... it works for gtests but fails for benchmark
 //  kNanosecond, kMicrosecond, kMillisecond, kSecond
 //BENCHMARK(add_asks_stl)->Arg(0)->Unit(benchmark::kMillisecond);
-BENCHMARK(add_mixed_stl)->Arg(0)->Unit(benchmark::kMillisecond);
+//BENCHMARK(add_mixed_stl)->Arg(0)->Unit(benchmark::kMillisecond);
 //BENCHMARK(add_asks_abseil)->Arg(0)->Unit(benchmark::kMillisecond);
+
+BENCHMARK(add_asks_stl)->Arg(0)->Unit(benchmark::kMillisecond);
+BENCHMARK(add_mixed_stl)->Arg(0)->Unit(benchmark::kMillisecond);
+BENCHMARK(create_then_close_orders_stl)->Arg(0)->Unit(benchmark::kMillisecond);
+BENCHMARK(store_stl)->Arg(0)->Unit(benchmark::kMillisecond);
+BENCHMARK(store_then_load_stl)->Arg(0)->Unit(benchmark::kMillisecond);
 
 BENCHMARK_MAIN();
